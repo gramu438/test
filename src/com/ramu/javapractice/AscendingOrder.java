6c9cf31f314bc416812a4478b7f655af100644fc
gramu438 <gramu438@gmail.com>
2019-10-16 20:57:59 +0530

    code update

---
+++ src/com/ramu/javapractice/AscendingOrder.java
@@ -0,0 +1,30 @@
+package com.ramu.javapractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeSet;
+
+public class AscendingOrder {
+
+	public static void main(String[] args) {
+		int[]a= {12,45,12,30,45,50,60};
+            /* TreeSet<Integer>treeSet=new TreeSet<>();
+             for(int i:a) {
+            	treeSet.add(i);
+             }
+             System.out.println(treeSet);*/
+		/*Arrays.sort(a);
+		for(int i:a) {
+			System.out.println(i);
+		}*/
+		List<Integer>list=new ArrayList<>();
+		for(int i:a) {
+			list.add(i);
+		}
+		Collections.sort(list);
+		System.out.println(list);
+	}
+
+}
